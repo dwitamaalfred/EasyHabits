25384d630803cd28cf7a3a55271f9c29e02b5a3e
dwitamaalfred <dwitama.alfred@gmail.com>
2022-01-16 13:52:38 +0800

    Initial Commit

---
+++ EasyHabits/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EasyHabits
+//
+//  Created by Dwitama Alfred on 16/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
